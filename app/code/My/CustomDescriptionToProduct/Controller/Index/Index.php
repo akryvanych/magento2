4ee7e22a43a1e0536c99a15444afb93c10c94416
akryvanych <and55580415@gmail.com>
2020-12-22 11:49:02 +0200

    Fix

--- app/code/My/CustomDescriptionToProduct/Controller/Index/Index.php
+++ app/code/My/CustomDescriptionToProduct/Controller/Index/Index.php
@@ -3,15 +3,20 @@ declare(strict_types=1);
 
 namespace My\CustomDescriptionToProduct\Controller\Index;
 
+use Magento\Framework\App\Action\Action;
+use Magento\Framework\App\Action\Context;
 use Magento\Framework\Controller\ResultFactory;
 use Magento\Framework\Webapi\Rest\Request;
 use My\CustomDescriptionToProduct\Api\CustomDescriptionToProductRepositoryInterface as CustomCommentRepository;
+
 use My\CustomDescriptionToProduct\Model\CustomDescriptionToProductFactory;
 
 /**
  * Sets extra comment to db and save.
+ *
+ * @SuppressWarnings(PHPMD)
  */
-class Index
+class Index extends Action
 {
     /**
      * @var Request
@@ -36,17 +41,20 @@ class Index
     /**
      * Controller constructor.
      *
+     * @param Context $context
      * @param ResultFactory $resultFactory
      * @param Request $request
      * @param CustomCommentRepository $commentRepository
      * @param CustomDescriptionToProductFactory $customDescriptionToProductFactory
      */
     public function __construct(
+        Context $context,
         ResultFactory $resultFactory,
         Request $request,
         CustomCommentRepository $commentRepository,
         CustomDescriptionToProductFactory $customDescriptionToProductFactory
     ) {
+        parent::__construct($context);
         $this->resultFactory = $resultFactory;
         $this->request = $request;
         $this->commentRepository = $commentRepository;
